49d290d036d713b19d3190fbfd173f6ad485fe97
Tyler Goodlet <tgoodlet@gmail.com>
2018-03-27 16:03:01 -0400

    Add brokermod loader utils

--- piker/brokers/__init__.py
+++ piker/brokers/__init__.py
@@ -1,3 +1,23 @@
 """
 Broker clients, daemons and general back end machinery.
 """
+from importlib import import_module
+from types import ModuleType
+
+__brokers__ = [
+    'questrade',
+    'robinhood',
+]
+
+
+def get_brokermod(brokername: str) -> ModuleType:
+    """Return the imported broker module by name.
+    """
+    return import_module('.' + brokername, 'piker.brokers')
+
+
+def iter_brokermods():
+    """Iterate all built-in broker modules.
+    """
+    for name in __brokers__:
+        yield get_brokermod(name)
