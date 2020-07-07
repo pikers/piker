a586edb27a67ff826e9d48f09d21b2a6624c69ae
Tyler Goodlet <jgbt@protonmail.com>
2020-07-07 10:39:22 -0400

    Docs the ui pkg mod

--- piker/ui/__init__.py
+++ piker/ui/__init__.py
@@ -1,3 +1,6 @@
 """
-Stuff for your eyes.
+Stuff for your eyes, aka super hawt Qt UI components.
+
+Currently we only support PyQt5 due to this issue in Pyside2:
+https://bugreports.qt.io/projects/PYSIDE/issues/PYSIDE-1313
 """
