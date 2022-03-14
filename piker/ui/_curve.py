803c65bc880d473e9ce894b3ab146b610dda373f
Tyler Goodlet <jgbt@protonmail.com>
2022-03-14 06:04:18 -0400

    Add no-path guard now that we can use a poly

--- piker/ui/_curve.py
+++ piker/ui/_curve.py
@@ -412,9 +412,8 @@ class FastAppendCurve(pg.PlotCurveItem):
     ) -> None:
 
         profiler = pg.debug.Profiler(
-            # disabled=False, #not pg_profile_enabled(),
+            disabled=not pg_profile_enabled(),
         )
-        # p.setRenderHint(p.Antialiasing, True)
 
         if (
             self._step_mode
@@ -434,13 +433,14 @@ class FastAppendCurve(pg.PlotCurveItem):
             profiler('.drawLine()')
             p.setPen(self.opts['pen'])
 
-        # else:
+        path = self.path
         if self._use_poly:
             assert self.poly
             p.drawPolyline(self.poly)
             profiler('.drawPolyline()')
-        else:
-            p.drawPath(self.path)
+
+        elif path:
+            p.drawPath(path)
             profiler('.drawPath()')
 
         # TODO: try out new work from `pyqtgraph` main which should
