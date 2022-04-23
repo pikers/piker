d244af69c931f797d6c9f86ea32703ece1585f91
Tyler Goodlet <jgbt@protonmail.com>
2022-04-23 17:30:27 -0400

    Don't require a symbol to  subcmd

--- piker/data/cli.py
+++ piker/data/cli.py
@@ -148,7 +148,7 @@ def storesh(
             enable_modules=['piker.data._ahab'],
         ):
             symbol = symbols[0]
-            await tsdb_history_update(symbol)
+            await tsdb_history_update()
 
     trio.run(main)
 
