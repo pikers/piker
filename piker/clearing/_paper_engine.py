453ebdfe30eda804c068e10b661190acfa11089d
Tyler Goodlet <jgbt@protonmail.com>
2022-06-25 18:42:06 -0400

    Fix field name to new `.bsuid`

--- piker/clearing/_paper_engine.py
+++ piker/clearing/_paper_engine.py
@@ -264,7 +264,7 @@ class PaperBoi:
             Symbol(key=symbol),
             size=pp_msg.size,
             be_price=pp_msg.avg_price,
-            uid=symbol.front_fqsn(),
+            bsuid=symbol,
         )
         pp_msg.size, pp_msg.avg_price = pp.lifo_update(size, price)
 
