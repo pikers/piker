db5aacdb9cde6b3f2cc71189add0328aae63f578
Tyler Goodlet <jgbt@protonmail.com>
2022-07-26 09:47:36 -0400

    Only allow vnc client connections from localhost

--- dockering/ib/run_x11_vnc.sh
+++ dockering/ib/run_x11_vnc.sh
@@ -2,15 +2,19 @@
 
 # start VNC server
 x11vnc \
-    -ncache_cr \
-    -listen localhost \
+    -listen 127.0.0.1 \
+    -allow 127.0.0.1 \
+    -autoport 3003 \
+    -no6 \
+    -noipv6 \
     -display :1 \
+    -bg \
     -forever \
     -shared \
     -logappend /var/log/x11vnc.log \
-    -bg \
-    -noipv6 \
-    -autoport 3003 \
+    -ncache_cr \
+    -ncache \
+
     # can't use this because of ``asyncvnc`` issue:
     # https://github.com/barneygale/asyncvnc/issues/1
     # -passwd 'ibcansmbz'
