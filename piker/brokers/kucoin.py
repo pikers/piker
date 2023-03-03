8e91e215b3d142d31308cad4d4f2011b42864114
Tyler Goodlet <jgbt@protonmail.com>
2023-03-03 16:24:44 -0500

    WIP - ensure `asyncio` pumps the event loop each send

--- piker/brokers/kucoin.py
+++ piker/brokers/kucoin.py
@@ -1,3 +1,6 @@
+# piker: trading gear for hackers
+# Copyright (C) Jared Goldman (in stewardship for pikers)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
