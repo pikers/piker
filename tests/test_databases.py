cda7a54718329771524662a9b64d3a3bdd6829f4
Tyler Goodlet <jgbt@protonmail.com>
2023-03-08 20:20:11 -0500

    Fix final missed `marketstore` mod import
    Thanks @esme! XD
    
    Also, do a linter pass and remove a buncha unused references.

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -1,25 +1,19 @@
-import pytest
 import trio
 
 from typing import AsyncContextManager
 
-from piker.service import Services
-from piker.log import get_logger
-
 from elasticsearch import Elasticsearch
-from piker.data import marketstore
+from piker.service import marketstore
+
 
 def test_marketstore_startup_and_version(
     open_test_pikerd: AsyncContextManager,
     loglevel,
 ):
-
     '''
     Verify marketstore starts correctly
 
     '''
-    log = get_logger(__name__)
-
     async def main():
         # port = 5995
 
@@ -36,7 +30,6 @@ def test_marketstore_startup_and_version(
                 len('3862e9973da36cfc6004b88172c08f09269aaf01')
             )
 
-
     trio.run(main)
 
 
@@ -49,8 +42,6 @@ def test_elasticsearch_startup_and_version(
 
     '''
 
-    log = get_logger(__name__)
-
     async def main():
         port = 19200
 
@@ -61,6 +52,5 @@ def test_elasticsearch_startup_and_version(
 
             es = Elasticsearch(hosts=[f'http://localhost:{port}'])
             assert es.info()['version']['number'] == '7.17.4'
-
 
     trio.run(main)
