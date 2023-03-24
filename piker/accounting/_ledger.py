485a17af262f855f43289a56bfc5c4477a0d6b2f
Tyler Goodlet <jgbt@protonmail.com>
2023-03-24 18:38:29 -0400

    Drop weird extra line from license headers

--- piker/accounting/_ledger.py
+++ piker/accounting/_ledger.py
@@ -12,8 +12,12 @@
 # GNU Affero General Public License for more details.
 
 # You should have received a copy of the GNU Affero General Public License
-
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+Trade and transaction ledger processing.
+
+'''
 from __future__ import annotations
 from contextlib import contextmanager as cm
 import os
